e476dafd531c6d423d6b90d705ac7fa4ec4059ea
Chigozie Oduah <oduahchigozie46@gmail.com>
2026-04-04 14:21:09 +0100

    Initial Commit

---
+++ SimpleEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleEngine
+//
+//  Created by Chigozie Oduah on 04/04/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
